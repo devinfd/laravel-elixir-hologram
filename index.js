022d4b33f7e640d2a7bc84b77da27fe3e8431b1c
Devin Dombrowski - Locutus <devin@devindombrowski.com>
2016-10-09 13:59:50 -0700

    upgraded to laravel-elixir 6.* and other improvements

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
-var gulp = require('gulp'),
-	Elixir = require('laravel-elixir'),
-	hologram = require('gulp-hologram'),
-	_ = require('underscore');
+var fs = require('fs');
+var gulp = require('gulp');
+var yaml = require('js-yaml');
+var elixir = require('laravel-elixir');
+var hologram = require('gulp-hologram');
 
-Elixir.extend('hologram', function(options) {
+elixir.extend('hologram', function(yamlFile) {
 
-	var defaultOptions = {
-		yml: './node_modules/laravel-elixir-hologram/config.yml'
-	};
+    yamlFile = yamlFile || './node_modules/laravel-elixir-hologram/config.yml';
 
-	options = _.extend(defaultOptions, options);
+    new elixir.Task('hologram', function() {
+        var err = function(e) {
+            new elixir.Notification().error(e, 'Hologram Failed!');
+            this.emit('end');
+        };
 
-	new Elixir.Task('hologram', function() {
-		gulp.src(options.yml)
-			.pipe(hologram())
-			.on('error', function(e) {
-                new Elixir.Notification().error(e, 'Hologram Failed!');
+        if(typeof this.recordStep !== 'undefined') {
+            var doc = yaml.safeLoad(fs.readFileSync(yamlFile, 'utf8'));
 
-                this.emit('end');
-            })
-			.pipe(new Elixir.Notification().message('Hologram Complete!'));
-	})
-	.watch([
-		config.get('assets.css.sass.folder') + '/**/*',
-		config.get('assets.css.less.folder') + '/**/*'
-	]);
+            this.recordStep('Building Style Guide');
+            this.src = doc.source;
+            this.output = doc.destination;
+        }
+
+        gulp.src(yamlFile)
+            .pipe(hologram())
+            .on('error', err)
+            .pipe(new elixir.Notification().message('Hologram Complete!'));
+    })
+
+    .watch([
+        elixir.config.get('assets.css.sass.folder') + '/**/*',
+        elixir.config.get('assets.css.less.folder') + '/**/*'
+    ]);
+
 });
